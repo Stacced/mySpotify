b50a0b042b015d6ccc12ec5a086d9290666d9c7a
Batman <20029433+Stacced@users.noreply.github.com>
2022-01-09 23:25:16 +0100

    feat: search component

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,17 +1,18 @@
 import Navigation from "./Navigation";
 import Grid from '@mui/material/Grid';
 import { CssBaseline } from "@mui/material";
+import Search from "./Search";
 
 const Dashboard = () => {
     return (
         <>
         <CssBaseline/>
         <Grid container sx={{ minHeight: '100vh' }}>
-            <Grid item xs={6} sm={4} md={2}>
+            <Grid item xs={6} sm={4} md={2} sx={{ borderRight: '1px solid darkgrey'}}>
                 <Navigation/>
             </Grid>
-            <Grid item xs={6} sm={8} md={10} sx={{ backgroundColor: 'black' }}>
-                
+            <Grid item xs={6} sm={8} md={10}>
+                <Search/>
             </Grid>
         </Grid>
         </>
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,5 +1,72 @@
-const Search = () => {
+import { useEffect, useState } from 'react';
+import { FormControl, Grid, InputAdornment, InputLabel, List, ListItem, Select, MenuItem, TextField, Typography, Box } from "@mui/material";
+import { Search as SearchIcon } from '@mui/icons-material';
+import { useSpotify } from '../hooks/spotify';
 
+const Search = () => {
+    const [search, setSearch] = useState({ value: '', category: 'artist' });
+    const [searchResults, setSearchResults] = useState([]);
+    const { callEndpoint } = useSpotify();
+
+    useEffect(() => {
+        if (search.value) {
+            const timeout = setTimeout(() => {
+                const path = `/search?q=${search.value}&type=${search.category}`;
+                callEndpoint({ path: path })
+                .then(res => {
+                    setSearchResults(res[search.category + 's'].items);
+                    console.log(res[search.category + 's']);
+                })
+                .catch(console.error);
+            }, 500);
+        }
+    }, [search]);
+
+    const handleSearch = e => {
+        setSearch({ ...search, value: e.target.value });
+    }
+
+    const handleSearchCategoryChange = e => {
+        setSearch({ ...search, category: e.target.value });
+    }
+
+    return (
+        <>
+        <Grid container justifyContent="center" paddingTop={2} sx={{ width: '100%' }}>
+            <TextField label="Spotify search" variant="standard" placeholder="Search..." onChange={handleSearch} value={search.value} sx={{ width: '30%' }}
+            InputProps={{
+                startAdornment: (
+                    <InputAdornment position="start">
+                        <SearchIcon/>
+                    </InputAdornment>
+                )
+            }}/>
+            <FormControl>
+                <InputLabel id="select-search-category-label">Catégorie</InputLabel>
+                <Select labelId="select-search-category-label" id="select-search-category" value={search.category} label="Catégorie" onChange={handleSearchCategoryChange}>
+                    <MenuItem value={'album'}>Album</MenuItem>
+                    <MenuItem value={'artist'}>Artiste</MenuItem>
+                    <MenuItem value={'playlist'}>Playlist</MenuItem>
+                    <MenuItem value={'track'}>Titre</MenuItem>
+                </Select>
+            </FormControl>
+        </Grid>
+        <Box id="results-container" sx={{ maxHeight: '90vh', overflow: 'auto' }}>
+            <List>
+                {
+                    searchResults
+                    .sort((a, b) => b.popularity - a.popularity) // Sort by popularity for artists, doesn't impact other categories
+                    .map(result => (
+                        <ListItem key={result.id}>
+                            { result.images[0]?.url && <img src={result.images[0].url} alt="Cover" width="200vw" /> }
+                            <Typography variant="h6" sx={{ paddingLeft: '2vw' }}>{result.name}</Typography>
+                        </ListItem>
+                    ))
+                }
+            </List>
+        </Box>
+        </>
+    )
 }
 
 export default Search;
